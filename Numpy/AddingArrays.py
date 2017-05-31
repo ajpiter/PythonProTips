53a0ae5c097170b2a4beaf2ca153664e1010d850
AJ Piter <amanda.piter@gmail.com>
2017-05-31 11:39:47 -0400

    Create AddingArrays.py

---
+++ Numpy/AddingArrays.py
@@ -0,0 +1,7 @@
+#When adding two python lists the values of the second list are just amended to the values of the first list.
+python_list + python_list 
+[1,2,3,1,2,3]
+
+#When adding two NumPy arrays the value of both lists are added to each other. 
+numpy_array + numpy_array
+array([2,4,6])
