0f31dc95c627aa3e477889237bda39adc291ccbb
AJ Piter <amanda.piter@gmail.com>
2017-05-31 12:25:48 -0400

    Update Histogram.py

--- DataVisualization/Histogram.py
+++ DataVisualization/Histogram.py
@@ -1,3 +1,21 @@
+#Use pandas and matplotlib to create a historgram. 
+
+import pandas as pd
+import matplotlib.pyplot as plt 
+filenamevariable = 'filename.txt'
+data = pd.read_csv(file, sep = '\t', comment = '#', na_values = 'nothing')
+print(data.head())
+
+#select one column in the dataframe to plot 
+pd.DataFrame.hist(data[['columnname']])
+
+#Create labels for the x and y axis 
+plt.xlabel('labelname')
+plt.ylabel('labelname(subname)')
+
+plt.show()
+
+#Histograms for data cleaning 
 #In addition to data visualization, the use histograms can be used as a way to spot outliers and errors in continous data and plan your data cleaning steps
 
 df.columnname.plot('hist')
