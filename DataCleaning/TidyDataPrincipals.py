aebe81dca188ac8d0eac5acea9dd9505d456553c
AJ Piter <amanda.piter@gmail.com>
2017-05-31 13:50:20 -0400

    Create TidyDataPrincipals.py

---
+++ DataCleaning/TidyDataPrincipals.py
@@ -0,0 +1,5 @@
+#Tidy Data is a standard way to organize data values within a dataset for data analysis. 
+#Principals
+#1. columns represent seperate variables (use pandas melt to fix) 
+#2. rows represent indivdual observations 
+#3. observational units form tables 
