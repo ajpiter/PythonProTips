2f40e21794edb74b576d63264a7fd0f0deb47dbf
AJ Piter <amanda.piter@gmail.com>
2017-05-31 11:50:16 -0400

    Create ShapeNumPyArrays.py

---
+++ Numpy/ShapeNumPyArrays.py
@@ -0,0 +1,8 @@
+#You could find out the shape of a NumPy array using 
+
+variable.shape
+
+#On the variable below you would return (2,5)
+variable = np.array([[1,1,1,1,1], [25,26,27,28,29]])
+variable.shape
+(2,5)
