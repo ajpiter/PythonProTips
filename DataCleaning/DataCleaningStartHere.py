9a2065c3456db94fc8248593c92a4dc3f860df31
AJ Piter <amanda.piter@gmail.com>
2017-06-12 11:41:49 -0400

    Create DataCleaningStartHere.py

---
+++ DataCleaning/DataCleaningStartHere.py
@@ -0,0 +1,7 @@
+""" 1. Data Cleaning has basic commands for looking at your data. 
+2. Frequency Counts tell you how many values, or missing values are in a column. 
+3. Data Types, gives you the steps to convert data in a column from one type to another. 
+4. Merge Data Frames, allows you to combine data sets based on common columns. 
+5. Concatenating, combines two data files. 
+6. Globbing combines multiple data files. 
+7. Regular Expressions, are used to match vaules to a specific format such as in money $20.17 or phone numbers (123) 456-7890.
