22d27895f74f5c70f721c5f64d1aa1f9c44dfd7e
AJ Piter <amanda.piter@gmail.com>
2017-06-13 10:12:32 -0400

    Create SummaryInfo.py

---
+++ Pandas/SummaryInfo.py
@@ -0,0 +1,19 @@
+#Indexes are labels for rows that permit fast lookup and relationship operators in pandas 
+
+import pandas as pd 
+type(Dataframe) 
+
+#tells you how many (rows, columns) 
+dataframe.shape
+
+#tells you the names of columns 
+dataframe.columns 
+
+#allows you to see the first five rows 
+database.head()
+
+#allows you to see the last five rows 
+database.tail()
+
+#returns index, columns labels, datatype and other information 
+info()
