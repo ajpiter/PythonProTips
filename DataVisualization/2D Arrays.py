1cfd7fb20276ffa0604796847349ac755d756471
AJ Piter <amanda.piter@gmail.com>
2017-06-13 12:37:03 -0400

    Create 2D Arrays.py

---
+++ DataVisualization/2D Arrays.py
@@ -0,0 +1,10 @@
+#Plotting 2D arrays, such as images and maps. 
+#NumPy arrays resemble lists but are uniform in datatype. 
+#NumPy arrays support calculations over the entire array. 
+
+#indexing a 2D array 
+A[index0, index1] 
+
+#Slicing 
+slice = start:stop:stride 
+# A slice of 2, samples every other entry. 
