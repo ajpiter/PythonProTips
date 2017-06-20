cc8bd31811431b59757d8880c395b0743f009ac1
AJ Piter <amanda.piter@gmail.com>
2017-06-20 12:34:47 -0400

    Update Histogram.py

--- DataVisualization/Histogram/Histogram.py
+++ DataVisualization/Histogram/Histogram.py
@@ -1,14 +1,20 @@
 #Use pandas and matplotlib to create a historgram. 
 
+#Histogram using one column from a file 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+variable = pd.read_csv('file.csv')
+variable.plot(kind = 'hist', y = 'columnname')
+plt.show()
+
+#Histogram using a tab deliminated file, were comments are indicated with "#' and missing values are 'nothing'
 import pandas as pd
 import matplotlib.pyplot as plt 
 filenamevariable = 'filename.txt'
-data = pd.read_csv(file, sep = '\t', comment = '#', na_values = 'nothing')
+data = pd.read_csv(filenamevariable, sep = '\t', comment = '#', na_values = 'nothing')
 print(data.head())
-
 #select one column in the dataframe to plot 
 pd.DataFrame.hist(data[['columnname']])
-
 #Create labels for the x and y axis 
 plt.xlabel('labelname')
 plt.ylabel('labelname(subname)')
