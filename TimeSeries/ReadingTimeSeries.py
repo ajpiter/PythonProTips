b1124002639e89e61a2959db2839882cc6a21697
AJ Piter <amanda.piter@gmail.com>
2017-06-20 15:46:08 -0400

    Update ReadingTimeSeries.py

--- TimeSeries/ReadingTimeSeries.py
+++ TimeSeries/ReadingTimeSeries.py
@@ -1,7 +1,11 @@
-#To use Pandas to read time series data, specifiy parse_dates =True
+#To use Pandas to read time series data, specifiy parse_dates=True
 #dates will be formated as yyyy-mm-dd hh:mm:ss
 
 import pandas as pd 
 variable = pd.read_csv('filename.csv', parse_dates=True, index-col='columnname')
 
-#you can use .head() to see the first five rows to see if you 
+#you can use .head() to see the first five rows or .info to get more details on the dataframe 
+variable.head()
+variable.info()
+
+
