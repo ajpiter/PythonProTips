34d0a0a0407ddbc1f6669aa8963694c6557e082a
AJ Piter <amanda.piter@gmail.com>
2017-06-20 13:10:28 -0400

    Update CustomizingHistograms.py

--- DataVisualization/Histogram/CustomizingHistograms.py
+++ DataVisualization/Histogram/CustomizingHistograms.py
@@ -3,3 +3,8 @@
 #range indicates the min and max of the bins 
 #normed is a boolean of whether to normalize a histogram 
 #cumulative is computes the Cumulative Distrubution Function (CDF) 
+
+#An example of a histogram using common customizations 
+
+variable.plot(y='columnname', kind='hist', bins=30, range=(4,6), normed=True) 
+plt.show()
