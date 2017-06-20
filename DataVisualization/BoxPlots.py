83c35e08cb2fcb4d901e2f77e9c996683281ca5d
AJ Piter <amanda.piter@gmail.com>
2017-06-20 12:18:43 -0400

    Update BoxPlots.py

--- DataVisualization/BoxPlots.py
+++ DataVisualization/BoxPlots.py
@@ -1,10 +1,20 @@
 #Box plots allow you to visualize summary statistics including outliers, min/max, 25th, 50th and 75th percentiles. 
-df.boxplot(column='columnames', by = 'columname')
+
+#To create a box plot using pandas 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+variable = pd.read_csv('file.csv') 
+variable.plot(y = 'columnname', kind = 'box')
+plt.xlabel('Label') 
+plt.ylabel('Label')
+plt.show()
+
+#or 
+variable.boxplot(column='columnames', by = 'columname')
 plt.show()
 
 #or
-
 import pandas as pd
 import matplotlib.pyplot as plt 
-df.boxplot(column='columnname',by= 'columnname', rot = 90)
+variable.boxplot(column='columnname',by= 'columnname', rot = 90)
 plt.show()
