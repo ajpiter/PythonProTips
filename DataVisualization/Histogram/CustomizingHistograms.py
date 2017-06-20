a5d147d98cda82e247d3f8dbd6a83948433369c6
AJ Piter <amanda.piter@gmail.com>
2017-06-20 12:35:13 -0400

    Create CustomizingHistograms.py

---
+++ DataVisualization/Histogram/CustomizingHistograms.py
@@ -0,0 +1,5 @@
+#Hisograms can be further customized typically through bins, range, normed or cumulative. 
+#bins indicate the number of intervals in the histogram 
+#range indicates the min and max of the bins 
+#normed is a boolean of whether to normalize a histogram 
+#cumulative is computes the Cumulative Distrubution Function (CDF) 
