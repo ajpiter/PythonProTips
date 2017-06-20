2f4c591587e6579a8a54fac12bd21f17517f165a
AJ Piter <amanda.piter@gmail.com>
2017-06-20 12:13:22 -0400

    Update ScatterPlots.py

--- DataVisualization/ScatterPlots.py
+++ DataVisualization/ScatterPlots.py
@@ -1,5 +1,18 @@
-#Scatter plots are best used to show the relationship bewteen two numeric variables or to flag potential errors not found by looking at one variable. 
+#Scatter plots are best used to show the relationship bewteen two numeric variables or 
+#to flag potential errors not found by looking at one variable. 
+#To create a scatter plot using pandas 
 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+variable = pd.read_csv('file.csv') 
+print(variable.shape)
+variable.head()
+varaibale.plot(x='columnname', y='columnname2', kind='scatter')
+plt.xlabel('Label') 
+plt.ylabel('Label')
+plt.show()
+
+#scatter plots can also have roatation 
 df.plot(kind = 'scatter', x = 'columnname', y = 'columnname', rot = 70)
 plt.show()
 
