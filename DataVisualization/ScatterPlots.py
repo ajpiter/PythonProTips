a844c223101d1468aed2122dbcea680aee232c9d
AJ Piter <amanda.piter@gmail.com>
2017-06-20 12:19:30 -0400

    Update ScatterPlots.py

--- DataVisualization/ScatterPlots.py
+++ DataVisualization/ScatterPlots.py
@@ -5,17 +5,15 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 variable = pd.read_csv('file.csv') 
-print(variable.shape)
-variable.head()
-varaibale.plot(x='columnname', y='columnname2', kind='scatter')
+variable.plot(x='columnname', y='columnname2', kind='scatter')
 plt.xlabel('Label') 
 plt.ylabel('Label')
 plt.show()
 
 #scatter plots can also have roatation 
-df.plot(kind = 'scatter', x = 'columnname', y = 'columnname', rot = 70)
+variable.plot(kind = 'scatter', x = 'columnname', y = 'columnname', rot = 70)
 plt.show()
 
 #Scatter Plot of a Subset 
-df_subset.plot(kind = 'scatter', x = 'columnname', y = 'columnname', rot = 70)
+variable_subset.plot(kind = 'scatter', x = 'columnname', y = 'columnname', rot = 70)
 plt.show()
