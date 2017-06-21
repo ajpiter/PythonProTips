9a8abd04af43087a71095aa67e60badba749f296
AJ Piter <amanda.piter@gmail.com>
2017-06-21 14:00:14 -0400

    Update FilteringTimesSeriesData.py

--- TimeSeries/FilteringTimesSeriesData.py
+++ TimeSeries/FilteringTimesSeriesData.py
@@ -14,16 +14,3 @@ database['Columnname'].str.contains('search')
 #In python the boolean value of True = 1 and False = 0 
 #We can add up all the true values in a column
 database['Columnname'].str.contains('search').sum()
-
-#To extract the hour of the day from the date column 
-datebase['columnname'].dt.hour 
-
-#To convert time zones 
-#first localize to the timezone that the data is in, in this case central 
-central = database['colunname'].dt.tz_localize('US/Central')
-central 
-#convert to US Eastern time zone 
-central.dt.tz_convert('US/Eastern')
-
-#OR you can localize and convert time zones in one line 
-database['columnname'].dt.tz_localize('US/Central').dt.tz_convert('US/Easterm')
