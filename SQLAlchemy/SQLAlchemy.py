c8a905ccc4cb41deda085a48271802586c18e7b0
AJ Piter <amanda.piter@gmail.com>
2017-06-21 11:30:14 -0400

    Update SQLAlchemy.py

--- SQLAlchemy/SQLAlchemy.py
+++ SQLAlchemy/SQLAlchemy.py
@@ -4,3 +4,4 @@
 #the ORM focused on data models and classes that a programmer creates
 
 #Many Types of databases including, SQLite, PostgreSQL, MySQL, MS SQL, and Oracle. 
+#Hides the differences bewteen database types so we can focus on the data that we want to work with instead of the differences in database types
