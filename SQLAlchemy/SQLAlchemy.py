1f10446446b73a8d76e963645bdd634c90a28193
AJ Piter <amanda.piter@gmail.com>
2017-06-21 10:12:07 -0400

    Create SQLAlchemy.py

---
+++ SQLAlchemy/SQLAlchemy.py
@@ -0,0 +1,6 @@
+#SQLAlchemy, allows you to generate SQL queries by writting python code. 
+#Two main pieces:
+#the Core which focuses on the relational model of the database
+#the ORM focused on data models and classes that a programmer creates
+
+#Many Types of databases including, SQLite, PostgreSQL, MySQL, MS SQL, and Oracle. 
