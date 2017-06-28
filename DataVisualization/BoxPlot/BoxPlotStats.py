5d5cbd63ce2c7701e7bbe131e22d06f117ab8201
AJ Piter <amanda.piter@gmail.com>
2017-06-28 14:51:41 -0400

    Create BoxPlotStats.py

---
+++ DataVisualization/BoxPlot/BoxPlotStats.py
@@ -0,0 +1,9 @@
+#Box plots visually show stats on a dataset 
+
+"""The box itself is bewteen the upper line, or 75th percentile, and the lower line of the 25th percentile. 
+The median or 50th percentile is drawn in the middle. 
+Outside the box, a line extends to the extent of the data, or to 1.5 Interquartile range (IQR) 
+The IQR is calculated with 1 IQR equal to the distance bewteen the 25th and 75th percentiles. 
+Any outliers, above the 1.5 IQR range are plotted as indivdual dots."""
+
+#Use seaborn to create a boxplot. 
