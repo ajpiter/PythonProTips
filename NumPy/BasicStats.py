ccca48f80eb5be1a59f9f9e659465da25cade620
AJ Piter <amanda.piter@gmail.com>
2017-06-28 14:45:31 -0400

    Update BasicStats.py

--- NumPy/BasicStats.py
+++ NumPy/BasicStats.py
@@ -2,3 +2,6 @@ np.mean(np_array[:,0])
 np.median(np_array[:,0])
 np.corrcoef(np_array[:,0], np_array[:,1])
 np.std(np_array[:,0])
+
+# for 1st and 3rd quartiles plus the median 
+np.percentile(dataset['columnname'], [25,50,75])
