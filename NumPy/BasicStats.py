4ecb0bfea77c1c744e2b3d84ba8c2ce17b9a50c7
AJ Piter <amanda.piter@gmail.com>
2017-06-28 16:29:56 -0400

    Update BasicStats.py

--- NumPy/BasicStats.py
+++ NumPy/BasicStats.py
@@ -5,3 +5,9 @@ np.std(np_array[:,0])
 
 # for 1st and 3rd quartiles plus the median 
 np.percentile(dataset['columnname'], [25,50,75])
+
+#The variance is the mean squared distance of the data from the mean, or a measure of the spread of the data
+np.var(columnname) 
+
+#computing the standard deviation, or the square root of the variance 
+np.std(columnname)
