dc4a95915cf9915dad4a0901ea13e33226d8357d
AJ Piter <amanda.piter@gmail.com>
2017-06-29 12:00:19 -0400

    Update Covariance.py

--- DataVisualization/ScatterPlots/Covariance.py
+++ DataVisualization/ScatterPlots/Covariance.py
@@ -1,3 +1,11 @@
+
+#How two quanites compare together using a scatter plot 
+plt.plot(columnname/1000, columnname, marker='.', linestyle='none')
+plt.margins(0.02)
+_ = plt.xlabel('Label')
+_ = plt.ylabel('Label')
+plt.show()
+
 #Covariance is a measure of how two quanities vary together 
 #If you have two datasets you can measure the mean of both datasets 
 #If you select a single point, you can measure the differences bewteen the mean and the point
@@ -10,12 +18,28 @@
 #If x is high while y is low, or y is high while x is low 
 #The covariance is negative and the data is negativly correlated
 
+# Compute the covariance matrix: covariance_matrix
+import numpy as np 
+covariance_matrix = np.cov(versicolor_petal_length, versicolor_petal_width)
+# Print covariance matrix
+print(covariance_matrix)
+# Extract covariance of length and width of petals: petal_cov
+petal_cov = covariance_matrix[0,1]
+# Print the length/width covariance
+print(petal_cov)
+
 #Pearson correlation coefficient 
 A measue of how two variables depend on each other, without any units 
 #Divide the covariance by the standard deviations of x and y variables 
 #Ranges from -1 to 1. a value of 0 means there is no correlation bewteen the data 
 
-#How two quanites compare together using a scatter plot 
-plt.plot(columnname/1000, columnname, marker='.', linestyle='none')
-_ = plt.xlabel('Label')
-_ = plt.ylabel('Label')
+def pearson_r(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    corr_mat = np.corrcoef(x,y)
+    # Return entry [0,1]
+    return corr_mat[0,1]
+# Compute Pearson correlation coefficient for I. versicolor: r
+r = pearson_r(versicolor_petal_length, versicolor_petal_width)
+# Print the result
+print(r)
