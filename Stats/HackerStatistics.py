660038430cda0c94de4cb49a38217127a4c8c36e
AJ Piter <amanda.piter@gmail.com>
2017-06-29 13:41:32 -0400

    Create HackerStatistics.py

---
+++ Stats/HackerStatistics.py
@@ -0,0 +1,5 @@
+""" You can use hacker statistics to determine probabilities. 
+Start by detemining how to simulate data, then simulate the data repeatedly. 
+The probability is approcimately the fraction of the trials with the outcome you are looking for. """
+
+#Examples include Coin Flip Experiment 
