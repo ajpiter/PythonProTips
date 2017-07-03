b3a15987ac84b1a1c6c8a9a3c04b7b6781442627
AJ Piter <amanda.piter@gmail.com>
2017-07-03 12:23:14 -0400

    Create ProbabilityMassFunction.py

---
+++ Stats/ProbabilityMassFunction.py
@@ -0,0 +1,10 @@
+"""Probablitity Mass Function 
+The set of probabilities of discrete outcomes 
+
+The discrete Uniform PMF 
+Example, a person rolling a die once. 
+the outcomes are discrete because only cetain values can be obtained (1 to 6) 
+Each result has the same probability of 1/6 
+
+The PMF is a property of a probability distribution. 
+A probability distribution is a mathematical description of outcomes."""
