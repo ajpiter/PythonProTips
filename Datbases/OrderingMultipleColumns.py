c64b0066c1a895d5c8efcde7969d49d49c7794c8
AJ Piter <amanda.piter@gmail.com>
2017-07-17 13:37:20 -0400

    Update OrderingMultipleColumns.py

--- Datbases/OrderingMultipleColumns.py
+++ Datbases/OrderingMultipleColumns.py
@@ -1,3 +1,7 @@
+#To order by multiple columns you can list multiple columns in the order_by()
+#columns will be sorted by the first column, and then by the second column if there are duplicates from the first 
+#Example
+
 stmt = select([tablename.columns.columnname, tablename.columns.columnname]) 
 stmt = stmt.order_by(tablename.columns.columnname, tablename.columns.columnname)
 results = connection.execute(stmt).fetchall()
