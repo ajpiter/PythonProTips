00dcd2425a5a8fa15992211668875ad590142031
AJ Piter <amanda.piter@gmail.com>
2017-07-20 16:46:03 -0400

    Update BooleanSeries.py

--- Pandas/BooleanSeries.py
+++ Pandas/BooleanSeries.py
@@ -11,32 +11,3 @@ df[(df.columnname >= 50) & (df.columnnameb < 200)]
 
 #combining filters based on either condition 
 df[(df.columnname >= 50) | (df.columnnameb < 200)]
-
-#DataFrames with zeros and NaNs 
-df2 = df.copy()
-df2['columnname'] = [ 0,0, 50, 60, 70, 80] 
-df2
-
-#select columns with all nonzeros 
-df2.loc[:, df2.all()] 
-
-#select columns with any nonzeros 
-df2.loc[:, df2.any()] 
-
-#select columns with any NaNs 
-df.loc[:, df.isnull().any()] 
-
-#select columns without Nans 
-df.loc[:, df.notnull().all()]
-
-#Drop rows with any NaNs 
-df.dropna(how = 'any') 
-
-#filtering a column based on another 
-df.columnname[df.columnnameb > 55] 
-
-#modifying a column based on another 
-df.columnname[df.columnnameb > 55] += 5 
-
-
-
