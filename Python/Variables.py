9f65c7b4ccd77806bd70c163f0cc093879011b56
AJ Piter <amanda.piter@gmail.com>
2017-07-24 15:20:22 -0400

    Create Variables.py

---
+++ Python/Variables.py
@@ -0,0 +1,9 @@
+#Variables are specific case-sensitive names 
+variable = number or value 
+
+#Below height and weight are examples of variables 
+height = 64
+weight = 125 
+
+#Example, To calculate BMI using variables and assign it to a new variable bmi 
+bmi = weight / height ** 2 
