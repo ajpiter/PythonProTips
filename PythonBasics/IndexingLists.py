60fd6dbf66e6908bc2dde9fc68ee1a06421c25be
AJ Piter <amanda.piter@gmail.com>
2017-07-24 16:11:18 -0400

    Create IndexingLists.py

---
+++ PythonBasics/IndexingLists.py
@@ -0,0 +1,14 @@
+#In python the index values of a list begin at zero.
+#This is known as zero-based indexing. 
+
+#For example, the following lists has indexes at the below values. 
+listnamevariable = [8.27, listvalue, True, listvalue2, 55.60, listvalue3, 1, False, 4] 
+#index:             0     1           2     3           4      5          6   7     8 
+
+#To subset a list using the index number 
+listnamevariable[3]
+#In this case would return listvalue2 
+
+#Lists can also be indexes from the end using negative numbers begining with -1
+listnamevariable[-1] 
+#would return 4 
