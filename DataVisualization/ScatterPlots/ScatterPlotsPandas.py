8286ac26d60f81f993e3ef2c7d94301f84eae922
AJ Piter <amanda.piter@gmail.com>
2017-07-25 15:56:39 -0400

    Update ScatterPlotsPandas.py

--- DataVisualization/ScatterPlots/ScatterPlotsPandas.py
+++ DataVisualization/ScatterPlots/ScatterPlotsPandas.py
@@ -1,7 +1,14 @@
 #Scatter plots are best used to show the relationship bewteen two numeric variables or 
 #to flag potential errors not found by looking at one variable. 
-#To create a scatter plot using pandas 
 
+#To create a scatter plot using matplotlib 
+import matplotlib.pyplot as plt 
+lista = [2001, 2002, 2003, 2004]
+listb = [1, 2, 3, 4] 
+plt.plot(lista, listb)
+plt.show()
+
+#To create a scatter plot using pandas 
 import pandas as pd 
 import matplotlib.pyplot as plt 
 variable = pd.read_csv('file.csv') 
