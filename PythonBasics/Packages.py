0afe0fbd6c78c8a2316c34449d61799195956cae
AJ Piter <amanda.piter@gmail.com>
2017-07-25 13:46:48 -0400

    Create Packages.py

---
+++ PythonBasics/Packages.py
@@ -0,0 +1,15 @@
+#Not all code is in the Python distribution. 
+#Thousands of packages are available, with common being Pandas, Numpy, Matplotlib, Scikit-learn. 
+#You can download packages indivdually or download a platform with multiple packages such as Anaconda. 
+
+#when using the package you can begin by importing the entire package and then calling on the methods you need like so: 
+import numpy 
+numpy.array([1,2,3]) 
+
+#you can import the package with a commonly used shorthand 
+import numpy as np 
+np.array([1,2,3]) 
+
+#or import specific methods of a function 
+from numpy import array 
+array([1,2,3])
