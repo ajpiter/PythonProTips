08f09e55a37e82808ab4fe102b8484378f2bcc72
AJ Piter <amanda.piter@gmail.com>
2017-07-25 15:54:24 -0400

    Update LinePlot.py

--- DataVisualization/LinePlot.py
+++ DataVisualization/LinePlot.py
@@ -7,3 +7,10 @@ print(variable.shape)
 variable.head()
 variable.plot(x='columnname', y='columnname2') 
 plt.show()
+
+#To create a line plot using Matplotlib 
+import matplotlib.pyplot as plt 
+lista = [2001, 2002, 2003, 2004]  
+listb = [1, 2, 3, 4] 
+plt.plot(lista, listb) 
+plt.show()
