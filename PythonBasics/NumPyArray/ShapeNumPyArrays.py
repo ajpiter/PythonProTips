66f5e1b08d999fa7c531bd9a224bab449799b22e
AJ Piter <amanda.piter@gmail.com>
2017-07-25 14:43:31 -0400

    Update ShapeNumPyArrays.py

--- PythonBasics/NumPyArray/ShapeNumPyArrays.py
+++ PythonBasics/NumPyArray/ShapeNumPyArrays.py
@@ -1,8 +1,10 @@
-#You could find out the shape of a NumPy array using 
+#You can create a 2D NumPy Array by 
+#variable = np.array([list one], [list two])
+variable = np.array([[1,1,1,1,1], [25,26,27,28,29]])
 
-variable.shape
-
-#On the variable below you would return (2,5)
+#You could find out the shape of a NumPy array using variable.shape
 variable = np.array([[1,1,1,1,1], [25,26,27,28,29]])
 variable.shape
-(2,5)
+# output (2,5) for two rows and five columns of data
+
+#Remember that numPy arrays have to be of the same type (str, list, int, bool etc) 
