e60f5e359c0fef876261b0deda60897a9688a47b
AJ Piter <amanda.piter@gmail.com>
2017-07-25 16:09:55 -0400

    Update Labels.py

--- DataVisualization/Labels.py
+++ DataVisualization/Labels.py
@@ -1,5 +1,16 @@
+
+#To label both axis and add a title. You can also choose to omit either label or the title by removing that line of code. 
 import matplotlib.pyplot as plt 
+lista = [2001, 2002, 2003, 2004] 
+listb = [1, 2, 3, 4] 
+plt.plot(lista, listb)
 plt.xlabel('Label on the X axis')
 plt.ylabel('Label on the Y axis') 
 plt.title('Title of the plot')
-plt.show
+plt.show()
+
+#To plot tick marks along your axis at specific intervals just add 
+plt.yticks([0,1,2,3,4,5]) 
+
+#Or to plot ticks with additional information 
+plt.yticks([0,1,2,3,4,5], ['0','1 Mil', '2 Mil', '3 Mil', '4 Mil', '5 Mil'])
