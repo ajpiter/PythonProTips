2ddbecda7eb7054e2480221c821c3efeb0325583
AJ Piter <amanda.piter@gmail.com>
2017-07-25 15:38:02 -0400

    Rename BasicStats.py to BasicsForDataframes.py

--- Stats/BasicsForDataframes.py
+++ Stats/BasicsForDataframes.py
@@ -1,6 +1,7 @@
 #Describe provides summary information on the dataset including: 
 #Count, Mean, Standard Deviation (std), min, 1st quartile, mean/2nd quartile, 3rd quartile, and max
 #There are multiple methods for calculating basic stats below.
+
 dataframe.describe()
 
 #Count is the number of entries 
@@ -10,18 +11,28 @@ dataframe['columnname'].count()
 #or multiple columns as a list 
 dataframe[['columnname', 'columnnameb', 'columnnamec']].count()
 
+#Min and Max 
+dataframe.min()
+dataframe.max()
+
 #Mean 
 dataframe.mean()
 #one column in a dataframe 
 dataframe['columnname'].count()
+#or use indexes to specifiy columns, and/or rows. 
+np.mean(dataframe[:,0]
+
+#Medians 
+dataframe.median()        
+#use numpy and indexes to specify columns, and/or rows. 
+np.median(dataframe[:,0])
 
 #Standard Deviations 
 dataframe.std()
 #one column in a dataframe
 dataframe['columnname'].std()
-
-#Medians 
-dataframe.median()
+#the standard deviation of a specific slice of a dataframe 
+np.std(dataframe[:,0])
 
 #Quartiles
 #1st Quartile
@@ -35,6 +46,6 @@ dataframe.quartile(q)
 q = [0.25, 0.75]
 dataframe.quartile(q)
 
-#Min and Max 
-dataframe.min()
-dataframe.max()
+#corrcoef
+np.corrcoef(dataframe[:, 0], dataframe[:,1])
+
