11dc666b870d3157a1da2d84cfafa106319cd1cc
AJ Piter <amanda.piter@gmail.com>
2017-07-27 15:08:08 -0400

    Create ForLoop.py

---
+++ PythonBasics/ForLoop.py
@@ -0,0 +1,24 @@
+sequence = [1, 2, 3, 4] 
+
+for var in sequence: 
+  print(var)
+#output 1 
+#output 2 
+#output 3 
+#output 4 
+
+for index, var in enumerate(sequence):
+  print('index ' +str(index) + ': " + str(var)
+#output index 0: 1 
+#output index 1: 2 
+#output index 2: 3 
+#output index 3: 4 
+
+for c in 'family':
+  print(c.capitalize())
+#Output F 
+#Output A 
+#Output M
+#Output I 
+#Output L 
+#Output Y 
