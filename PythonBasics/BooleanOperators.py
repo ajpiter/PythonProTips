01fe891e4f8db0c3473d565a385d0e94b0d92a99
AJ Piter <amanda.piter@gmail.com>
2017-07-27 14:16:59 -0400

    Create BooleanOperators.py

---
+++ PythonBasics/BooleanOperators.py
@@ -0,0 +1,33 @@
+#Boolean operators are and, or, not 
+
+True and True 
+#output is True 
+
+False and True 
+#output is False 
+
+False and False 
+#output is False 
+
+x = 12 
+x > 5 and x < 15 
+#output is True 
+
+True or True 
+#output is True 
+
+False or True 
+#output is True 
+
+False or False 
+#output is False 
+
+y = 5 
+y < 7 or y > 13 
+#output is True 
+
+not True 
+#output is False 
+
+not False 
+#output is True 
