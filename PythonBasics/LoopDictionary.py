36e3330a0a51fa293d6a79ed9a5b3dfaa6281e0b
AJ Piter <amanda.piter@gmail.com>
2017-07-27 15:11:11 -0400

    Create LoopDictionary.py

---
+++ PythonBasics/LoopDictionary.py
@@ -0,0 +1,7 @@
+world = {'country': 1, 'countryb':2, 'countryc':3}
+for key, value in world.items():
+  print(key + "--" + str(value))
+#output country -- 1 
+#output countryb -- 2 
+#output countryc -- 3
+
