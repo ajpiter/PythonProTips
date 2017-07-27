cc8e506dfd2166d721c9f5862cf688040622ad94
AJ Piter <amanda.piter@gmail.com>
2017-07-27 14:13:27 -0400

    Create Comparisons.py

---
+++ PythonBasics/Comparisons.py
@@ -0,0 +1,29 @@
+2 < 3 
+#output True 
+
+2 == 3 
+# output False 
+
+2 <= 3 
+#output True 
+
+2 != 3 
+#output True
+
+3 <= 3 
+#output True 
+
+x = 2 
+y = 3 
+x < y 
+#output True 
+
+'carl' < 'chris'
+#output True 
+
+3 < 'chris'
+#error message 
+
+3 < 4.1
+#output True 
+
