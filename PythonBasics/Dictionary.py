988757c8a3fd3230ae7e1d6feb199e0be08992f2
AJ Piter <amanda.piter@gmail.com>
2017-07-27 13:46:33 -0400

    Create Dictionary.py

---
+++ PythonBasics/Dictionary.py
@@ -0,0 +1,12 @@
+#unlike a list where order matters to select subsets, a dictionary is a lookup table with unique keys. 
+
+value = [1, 2, 3, 4] 
+key = ['a', 'b', 'c', 'd']
+dictionary = {'a':1, 'b':2, 'c':3, 'd':4}
+
+#then you can find the corresponding value using the key 
+dictionary['a']
+#output would be 1 
+
+dictionary
+#output would be {'a':1, 'b':2, 'c':3, 'd':4}
