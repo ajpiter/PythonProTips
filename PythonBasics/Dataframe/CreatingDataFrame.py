b08d259df61902a91bf18a1aecb240d925ed213b
AJ Piter <amanda.piter@gmail.com>
2017-07-27 14:01:49 -0400

    Update CreatingDataFrame.py

--- PythonBasics/Dataframe/CreatingDataFrame.py
+++ PythonBasics/Dataframe/CreatingDataFrame.py
@@ -1,5 +1,4 @@
 #A DataFrame is a 2 dimensional numPy array, whose columns are series. 
-
 #Dataframes can be built from Dictionaries. Below we construct a dictionary of lists. 
 
 import pandas as pd 
@@ -18,3 +17,17 @@ listb = ['a','b','c','d']
 listlabels = ['list','listb']
 #contains the column entries for each column 
 listcols = [list, listb] 
+
+#When outputed the rows of a dataframe are observations and the columns are variables. 
+#The keys of a dictionary are the column labels. 
+#To add row labels call dictionary.index = ['label', 'labelb', 'labelc', labeld']
+
+variable 
+dictionary = { 
+ "country":["Brazil", "Russia", "India", "China", "South Africa"],
+ "capital":["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
+ "area":[8.516, 17.10, 3.286, 9.597, 1.221]
+ "population":[200.4, 143.5, 1252, 1357, 52.98] }
+import pandas as pd
+variable = pd.DataFrame(dictionary) 
+dictionary.index = ['label', 'labelb', 'labelc', 'labeld']
