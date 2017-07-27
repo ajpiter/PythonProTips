699f994ab99a70ce193f87a0fa0aeee0977140db
AJ Piter <amanda.piter@gmail.com>
2017-07-27 14:59:44 -0400

    Create WhileLoop.py

---
+++ PythonBasics/WhileLoop.py
@@ -0,0 +1,10 @@
+# while is a numerically calculating model and repeatedly runs until a condition is met 
+
+error = 50.0 
+while error > 1: 
+  error = error / 4 
+  print(error) 
+  
+#output 12.5 
+#output 3.125 
+#output 0.78125 
