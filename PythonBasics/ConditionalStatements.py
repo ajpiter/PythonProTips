bccebaa0ba9826b965ce3f502612a8c8581de9ff
AJ Piter <amanda.piter@gmail.com>
2017-07-27 14:31:08 -0400

    Create ConditionalStatements.py

---
+++ PythonBasics/ConditionalStatements.py
@@ -0,0 +1,23 @@
+#conditional statements are if, else, elif statements 
+
+z = 4 
+if z % 2 == 0: 
+  print('z is even')   
+#output z is even
+
+z = 5 
+if z % 2 == 0: 
+  print('z is even')
+else: 
+  print('z is odd') 
+#output z is odd 
+
+z = 3 
+if z % 2 == 0: 
+  print('z is divisible by 2') 
+elif z % 3 == 0:
+  print('z is divisible by 3') 
+else:
+  print('z is neither divisible by 2 nor by 3')
+#output z is divisible by 3 
+
