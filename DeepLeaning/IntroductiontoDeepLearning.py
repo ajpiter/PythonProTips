95686a24e08dc342bf9ab81b2a07b8e4eea5991f
AJ Piter <amanda.piter@gmail.com>
2017-07-31 12:01:35 -0400

    Create IntroductiontoDeepLearning.py

---
+++ DeepLeaning/IntroductiontoDeepLearning.py
@@ -0,0 +1,22 @@
+#Introduction to Deep Learning 
+
+#Simple Linear Regression model, is based on the outcome being the sum of indivdual parts. 
+#The Linear Regression model does not identify the interactions bewteen the indivdual parts. 
+
+#Example of a Linear Regression Model trying to determine the number of transactions a customer at a bank will make. 
+#Indivudal parts taken into consideration are Age, Bank Balance, Retirement Status. 
+#A graph depicting a linear regression model of possible transactions would include a 
+#x axes with bank balance
+#y axes with predicted transactions 
+#parell line segments of retired and not retired customers 
+
+#Neural networks account for interactions and would allow the lines, of retired and not retired customers to be unparrell
+#Deep Learning is the use of neural networks 
+
+#Diagram for a neural network 
+#Far Left - Input Layer Representing Predictive Features (Age, Income, Number of Accounts)
+#Center - Hidden Layer made up of Nodes representing an aggregation of data from the input layer
+#Far Right - Output Layer Representing the Predections (Number of Transactions) 
+
+#Nodes in the hidden layer add to the models abilities to caputre interactions. 
+#The more nodes we have the more interactions we capture. 
