41e535ab6e9a190ecf234bd082033cd2c5dded33
AJ Piter <amanda.piter@gmail.com>
2017-08-01 10:10:37 -0400

    Create RepresentationLearningImages.py

---
+++ DeepLeaning/RepresentationLearningImages.py
@@ -0,0 +1,12 @@
+#In Deep Learning, also called Representation Learning, a modeler dosen't need to specify interactions. 
+#Deep networks internally build representations of patterns in the data 
+#subsequent layers build increasingly sophicated representations of raw data 
+
+#Applied to images: 
+#the first node in a hidden layer may be looking for lines, 
+#the second node for diganl lines, 
+#later hidden layers identify larger patterns such as boxes 
+#eventually the neural network is able to classifiy what the image is 
+
+#Modeler doesn't need to specify the interactions 
+#When you train the model the neural network gets weights that find the relevant patterns to make better predictions 
