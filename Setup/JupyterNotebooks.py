5e3958a2f7c751ee38320dad85de6825c7701c55
AJ Piter <amanda.piter@gmail.com>
2017-08-02 14:35:38 -0400

    Update JupyterNotebooks.py

--- Setup/JupyterNotebooks.py
+++ Setup/JupyterNotebooks.py
@@ -1,16 +1,21 @@
 #Launch Jupyter notebooks in your terminal by writting: 
 jupyter notebook 
-#If the link does not automatically launch copy the link to your browser 
-
-#Create a new notebook using either Python 3 or Python 2. 
-#You may also see other kernals here. 
-
-#Files shows all the files and folders in the current directory. 
-#Running shows all the currently running notebooks and allows you to manage them. 
 
 #Install Notebook Conda to help manage your environments: 
 conda install nb_conda
 #If you run the notebook server from a conda environment you'll also have access to the Conda tab. 
 #The Conda tab allows you to manage your environments within Jupyter. 
 
+#Shutdowning Indivdual Notebooks 
+#In the browser click the checkbox and click shutdown
+#shutdown the entire server 
+Control C, Control C 
 
+#Notebook Interface 
+#Cells are in green boxes, and where you will write your code. 
+#The play button runs the code in the cell, and displays the output below the cell. 
+#Cells get numbered based on when you ran the code.
+#Running the cell in markdown mode creates text. 
+#Command Palette allows you to search for what you want to do. 
+
+#Any code executed in one cell is available in all other cells 
