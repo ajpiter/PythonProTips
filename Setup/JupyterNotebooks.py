4554280be4a501e9071f53f2fc7b2db07da507d3
AJ Piter <amanda.piter@gmail.com>
2017-08-02 14:07:17 -0400

    Create JupyterNotebooks.py

---
+++ Setup/JupyterNotebooks.py
@@ -0,0 +1,16 @@
+#Launch Jupyter notebooks in your terminal by writting: 
+jupyter notebook 
+#If the link does not automatically launch copy the link to your browser 
+
+#Create a new notebook using either Python 3 or Python 2. 
+#You may also see other kernals here. 
+
+#Files shows all the files and folders in the current directory. 
+#Running shows all the currently running notebooks and allows you to manage them. 
+
+#Install Notebook Conda to help manage your environments: 
+conda install nb_conda
+#If you run the notebook server from a conda environment you'll also have access to the Conda tab. 
+#The Conda tab allows you to manage your environments within Jupyter. 
+
+
