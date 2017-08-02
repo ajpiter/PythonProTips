9906c9e8252d39f7a4d010976b53e12375b15b07
AJ Piter <amanda.piter@gmail.com>
2017-08-02 15:26:21 -0400

    Create JupyterNotebookShortcuts.py

---
+++ Setup/JupyterNotebookShortcuts.py
@@ -0,0 +1,33 @@
+#From Udacity's Intro to Deep Learning 
+
+#In Jupyter Notebook 
+
+#Linking in Markdown is done by enclosing text in square brackets and the URL in parentheses, like this 
+[Udacity's home page](https://www.udacity.com) for a link to Udacity's home page.
+
+#For italics, wrap the text in one asterisk or underscore, 
+_gelato_ or *gelato* renders as gelato.
+
+#Bold text uses two symbols, 
+**aardvark** or __aardvark__ looks like aardvark.
+
+#to display code, inline with text 
+#wrap the text in backticks
+`string.punctuation`
+#To create a code block
+```
+import requests
+response = requests.get('https://www.udacity.com')
+```
+
+#Math Expressions 
+#Wrap the expression in $ 
+$y = mx +b$
+#For a math block use double dollar signs 
+$$
+y = \frac{a}{b+c}
+$$
+
+#Keyboard Shortcuts - Jupyter Notebook 
+http://localhost:8890/notebooks/Desktop/keyboard-shortcuts.ipynb
+#Press h in command mode 
