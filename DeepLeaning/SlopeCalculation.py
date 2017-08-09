940650eb5e5a52fc26397e85f9ca8d3ce89c6866
AJ Piter <amanda.piter@gmail.com>
2017-08-09 10:37:04 -0400

    Update SlopeCalculation.py

--- DeepLeaning/SlopeCalculation.py
+++ DeepLeaning/SlopeCalculation.py
@@ -1,9 +1,9 @@
-#Slope Calculation Example 
-Node1 = 3 
+#Slope Calculation Example, with one input and no activation function. 
+Node1 = Input = 3 
 Weight = 2 
-Node2 = 6 
+Node2 = Output = 6 
 PredictedValue = Node2
-Actual Target Value = 10 
+ActualTargetValue = 10 
 LearningRate = 0.01 
 
 SlopePrediction = Slope of Mean-Squared loss function precdiction 
@@ -20,3 +20,27 @@ Slopeweight = -24
 
 NewWeight = Weight -LearningRate *SlopeRate
 NewWeight = 2 - 0.01(-24) 
+
+#Slope Calculation Example, with two inputs and no activation function
+#Code to calucalte the slopes and update the weights
+Node1 = 3 
+Node2 = 4 
+Weight1 = 1
+Weight2 = 2 
+
+import numpy as np 
+weights = np.array([1, 2]) 
+inputdata = np.array([3, 4]) 
+targetvalue = 6 
+learningrate = 0.01
+slopeprediction = (weights * inputdata).sum()
+error = slopeprediction - targetvalue 
+print(error) 
+gradient = 2 * inputdata * error 
+gradient 
+#returns ([30,40}) 
+weightsupdated = weights - learningrate * gradient 
+predictionupdated = (weightsupdated * inputdata).sum()
+errorupdated = predictionupdated - target 
+print(errorupdated) 
+#returns -2.5 
