a79fa67b3a0e0b261f97a4a1f8c1285f2b93acb6
AJ Piter <amanda.piter@gmail.com>
2017-08-09 10:39:08 -0400

    Create LearningRate.py

---
+++ DeepLeaning/LearningRate.py
@@ -0,0 +1,7 @@
+#Learning Rate 
+#If the slope is positve, then going opposite the slope means moving to lower numbers
+#Could Subtract the slope from the current value but we don't take too big of a step 
+#Instead we multiply the slope by a small number called a learning rate 
+#Change the weight by the product of that multiplication 
+#Weight - (LearningRate * Slope)
+#Standard Learning Rate is 0.01
