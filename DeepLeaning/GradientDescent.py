6a0a2836a6213088d7dfbb427fc0de0778ffc835
AJ Piter <amanda.piter@gmail.com>
2017-08-09 10:42:24 -0400

    Update GradientDescent.py

--- DeepLeaning/GradientDescent.py
+++ DeepLeaning/GradientDescent.py
@@ -6,14 +6,6 @@
 #then make a small change to the weight to get to a lower point 
 #repeat until you cannot go down hill anymore 
 
-#Learning Rate 
-#If the slope is positve, then going opposite the slope means moving to lower numbers
-#Could Subtract the slope from the current value but we don't take too big of a step 
-#Instead we multiply the slope by a small number called a learning rate 
-#Change the weight by the product of that multiplication 
-#Weight - (LearningRate * Slope)
-#Standard Learning Rate is 0.01
-
 #Use Keras and Tensorflow to do the calucus for you 
 
 #Slope Caluclation Example 
@@ -23,19 +15,15 @@ Node2 = 6
 PredictedValue = Node2
 Actual Target Value = 10 
 LearningRate = 0.01 
-
 SlopePrediction = Slope of Mean-Squared loss function precdiction 
 SlopePrediction = 2 * (Predicted Value - Actual Value) = 2*Error 
 SlopePrediction = 2 *(6 - 10) = 2 * -4
-
 Node1 = Value of the node that feeds into our weight
 Node1 = 3 
-
 SlopeWeight = Slope for a weight 
 SlopeWeight = SlopePrediction * Node1 
 SlopeWeight = 2 * -4 * 3 
 Slopeweight = -24 
-
 NewWeight = Weight -LearningRate *SlopeRate
 NewWeight = 2 - 0.01(-24) 
 
