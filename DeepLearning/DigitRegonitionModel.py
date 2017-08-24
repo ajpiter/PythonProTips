e3370b86eb734cae9f727e24b1155ce95766fb87
AJ Piter <amanda.piter@gmail.com>
2017-08-24 15:32:28 -0400

    Create DigitRegonitionModel.py

---
+++ DeepLearning/DigitRegonitionModel.py
@@ -0,0 +1,19 @@
+#Smaller than most digit regonition models online, but will provide 90% accuracy with less computing time. 
+
+# Create the model: model
+model = Sequential()
+
+# Add the first hidden layer
+model.add(Dense(50, activation='relu', input_shape=(784,)))
+
+# Add the second hidden layer
+model.add(Dense(50, activation='relu'))
+
+# Add the output layer
+model.add(Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X, y, validation_split=0.3)
