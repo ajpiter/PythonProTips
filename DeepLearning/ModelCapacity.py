d854ee130b6b27d1828c4836c1db966d6cc4e3e2
AJ Piter <amanda.piter@gmail.com>
2017-08-24 14:58:51 -0400

    Create ModelCapacity.py

---
+++ DeepLearning/ModelCapacity.py
@@ -0,0 +1,19 @@
+#Also known as network capacity 
+
+#Overfitting is the ability to fit oddities or outliers in your data 
+#overfitting makes accurate predictions on training data but inaccurate predictions on validation and new data sets 
+
+#underfitting is when your model fails to find important predicitive patterns 
+#underfitting is inaccurate in both the training data and validation data 
+
+#validation score is the measure of a models predicitive quality 
+
+#model capacity is the ability to capture predicitive patterns in data 
+
+#adding node/neruons increases the model capacity 
+#adding layers increases the model capcity 
+
+#Workflow
+#A. Start with a small network 
+#B. Get the validation score 
+#C. Keep increasing capacity until the validation score is no longer improving 
