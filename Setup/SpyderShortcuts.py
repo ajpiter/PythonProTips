778bd34eda83b840bf46d711f1f993ffb5da4823
AJ Piter <amanda.piter@gmail.com>
2021-01-08 23:15:22 -0500

    Create SpyderShortcuts.py

---
+++ Setup/SpyderShortcuts.py
@@ -0,0 +1,27 @@
+#Shortcuts in Spyder 
+
+Ctrl-Enter* executes the current cell (menu entry Run ‣ Run cell). A cell is defined as the code between two lines which start with the characters #%%, # %% or # <codecell>.
+
+Shift-Enter* executes the current cell and advances the cursor to the next cell (menu entry Run ‣ Run cell and advance).
+
+Cells are useful for breaking large files or long blocks of code into more manageable chunks. Like those in an IPython notebook, each cell can be run independently.
+
+Alt-Up* moves the current line up. If multiple lines are highlighted, they are moved up together. Alt-Down* works correspondingly, moving line(s) down.
+
+Ctrl-LeftMouseButton or Alt-G* on a function/method in the Editor opens a new Editor tab showing the definition of that function.
+
+Shift-Ctrl-Alt-M* maximizes the current window (or changes the size back to normal if pressed in a maximized window).
+
+Ctrl-Shift-F* activates the Find in Files pane, allowing grep-like searches across all files in a specified scope.
+
+Ctrl - = will increase the font size in the Editor or the Console, whereas Ctrl - - will decrease it.
+
+The font face and size for other parts of the UI can be set under Preferences ‣ General ‣ Appearance ‣ Fonts.
+
+Ctrl-S* in the Editor saves the file currently being edited. This also forces various warning triangles in the left column of the Editor to be updated (otherwise they update every 2.5 seconds by default, which is also configurable).
+
+Ctrl-S* in the Console saves the current IPython session as an HTML file, including any figures that may be displayed inline. This is useful as a quick way of recording what has been done in a session.
+
+(It is not currently possible to load this saved record back into the session -- if you need functionality like this, look for the IPython Notebook).
+
+Ctrl-I* when pressed while the cursor is on an object opens documentation for that object in the help pane.
