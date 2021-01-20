8bdca26f47b65cdea09cb739c93735b0f7d2a262
AJ Piter <amanda.piter@gmail.com>
2021-01-20 03:04:57 -0500

    Update ConditionalStatements.py

--- PythonBasics/ConditionalStatements.py
+++ PythonBasics/ConditionalStatements.py
@@ -20,6 +20,15 @@ else:
   print('z is odd') 
 #output z is odd 
 
+#Do the values match? 
+d1 = 1.11 - 1.10
+d2 = 2.11 - 2.10
+print('d1 =', d1, ' d2 =', d2)
+if d1 == d2:
+    print('Same')
+else:
+    print('Different')
+
 ----- #elif Statement ----- 
 z = 3 
 if z % 2 == 0: 
