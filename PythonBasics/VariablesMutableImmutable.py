2ebb2811349f5fe8f7bc3daa609274a30b495f03
AJ Piter <amanda.piter@gmail.com>
2021-01-20 02:23:38 -0500

    Create VariablesMutableImmutable.py

---
+++ PythonBasics/VariablesMutableImmutable.py
@@ -0,0 +1,9 @@
+## Immutability 
+
+#Generally means that the objects cannot be changed after they are created. 
+
+# In Python, the datatypes integer, float, and string (and tuple) are “immutable.”
+
+![image](https://user-images.githubusercontent.com/28680575/105132518-0ee0df80-5ab9-11eb-9712-f25766a3266b.png)
+#Source: Medium, Mutable vs Immutable Objects in Python
+#https://medium.com/@meghamohan/mutable-and-immutable-side-of-python-c2145cf72747
