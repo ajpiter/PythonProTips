a8f2d99996c1bc8a96c05074620088baef45fb26
AJ Piter <amanda.piter@gmail.com>
2021-01-26 02:27:21 -0500

    Create DataTypes.py

---
+++ PythonBasics/Lists/DataTypes.py
@@ -0,0 +1,19 @@
+#Lists can contain any datatype or combination of datatypes 
+
+#Floats or real numbers List
+float = [1.25, 15.99, 21.33]
+
+#Integer List
+int = [1, 2, 3, 4]
+
+#Strings List
+str = ["a", "b", "c"]
+
+#Boolean List
+bool = [True, False, True, True]
+
+#Mixed List
+mix = [True, 4, "a", 15.99]
+
+#List of Lists
+List = [(1.25, 15.99, 21.33), (1, 2, 3), ("a", "b", "c"), (True, False, True), (True, 4, "a")]
