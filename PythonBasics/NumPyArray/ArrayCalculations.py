7a86d2b661d3723d6e9e6b28a7128b63562a4dc8
AJ Piter <amanda.piter@gmail.com>
2021-01-26 00:02:15 -0500

    Update and rename LoopNumPyArrays.py to ArrayCalculations.py

--- PythonBasics/NumPyArray/ArrayCalculations.py
+++ PythonBasics/NumPyArray/ArrayCalculations.py
@@ -1,5 +1,6 @@
 #Examples taken from DataCamp 
 
+----- #Example: Basic Math with Indivdual Outputs ----- 
 import numpy as np
 np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
 np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
@@ -14,6 +15,7 @@ for val in bmi :
 #24.747
 #21.441
 
+----- #Example: Basic Math with Array Outputs ----- 
 import numpy as np
 np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
 np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
@@ -24,3 +26,11 @@ for val in meas :
 #Output
 #[ 1.73 1.68 1.71 1.89 1.79]
 #[ 65.4 59.2 63.6 88.4 68.7]
+
+----- #Example: Comparisons with Array Outputs ----- 
+import numpy as np
+np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
+np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
+bmi = np_weight / np_heigh ** 2 
+bmi > 23
+#[output] array([False, False, True, False], dtype=bool) 
