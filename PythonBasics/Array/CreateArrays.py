0a4ee1e887467ef0dc64a0ab6a1f9f8c371ca7b6
AJ Piter <amanda.piter@gmail.com>
2021-01-26 03:01:02 -0500

    Update CreateArrays.py

--- PythonBasics/Array/CreateArrays.py
+++ PythonBasics/Array/CreateArrays.py
@@ -1,4 +1,19 @@
-import numpy as np 
+
+#The NumPy package is an easy and fast alternative to python lists and can make calculations over entire arrays. 
+#Convert to a NumPy array when doing calculations bewteen two different types, such as list and int. 
+#NumPy arrays contain only one type 
+
+#Import the whole NumPy package. 
+import numpy as np
+np.array([1,2,3])
+
+#Import just the array. 
+from numpy import array 
+varaiable = array(file)
+
+#Import with a clear use of package 
+import numpy 
+varaiable = numpy.array(file)
 
 #You can create a 2D NumPy Array by 
 #variable = np.array([list one], [list two])
