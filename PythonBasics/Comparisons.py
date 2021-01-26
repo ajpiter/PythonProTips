4d5abba36653313437d8f1fcc93731895cb1e629
AJ Piter <amanda.piter@gmail.com>
2021-01-26 00:03:19 -0500

    Update Comparisons.py

--- PythonBasics/Comparisons.py
+++ PythonBasics/Comparisons.py
@@ -1,3 +1,5 @@
+#Comparions use == for equal, where = is used in Python to assign variables 
+
 2 < 3 
 #output True 
 
@@ -27,3 +29,9 @@ x < y
 3 < 4.1
 #output True 
 
+import numpy as np
+np_height = np.array([1.73, 1.68, 1.71, 1.89, 1.79])
+np_weight = np.array([65.4, 59.2, 63.6, 88.4, 68.7])
+bmi = np_weight / np_heigh ** 2 
+bmi > 23
+#[output] array([False, False, True, False], dtype=bool) 
