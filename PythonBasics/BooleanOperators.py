90ddd267e4fca851f01e601064472774f34fd45d
AJ Piter <amanda.piter@gmail.com>
2021-01-26 00:09:54 -0500

    Update BooleanOperators.py

--- PythonBasics/BooleanOperators.py
+++ PythonBasics/BooleanOperators.py
@@ -2,38 +2,36 @@
 #Numpy also uses logical_and(), logical_or(), and logical_not()
 
 ----- #Example: and -----
+True and True #output is True 
 
-True and True 
-#output is True 
+False and True #output is False 
 
-False and True 
-#output is False 
-
-False and False 
-#output is False 
+False and False #output is False 
 
 x = 12 
 x > 5 and x < 15 
 #output is True 
 
 ----- #Example: or ----- 
-True or True 
-#output is True 
+True or True #output is True 
 
-False or True 
-#output is True 
+False or True #output is True 
 
-False or False 
-#output is False 
+False or False #output is False 
 
 y = 5 
 y < 7 or y > 13 
 #output is True 
 
 ----- #Example: not ----- 
+not True #output is False 
 
-not True 
-#output is False 
+not False #output is True 
 
-not False 
-#output is True 
+---- #Example: logical_and() ----- 
+np.logical_and(variable > 21, variable < 25) 
+#[output] TRUE
+np.logival_and(array > 21, array < 25) 
+#[output] array([True, False, True, True], dtype=bool)
+
+
