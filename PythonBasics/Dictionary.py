f9e8ea98c62ffa5d9d0bc80dced225bd2121c235
AJ Piter <amanda.piter@gmail.com>
2021-01-25 23:45:03 -0500

    Update Dictionary.py

--- PythonBasics/Dictionary.py
+++ PythonBasics/Dictionary.py
@@ -10,3 +10,24 @@ dictionary['a']
 
 dictionary
 #output would be {'a':1, 'b':2, 'c':3, 'd':4}
+
+#Use index() to look up values 
+dictionary.index('b') 
+#output would be 2
+
+#Check if key exists in dictionary 
+"VariableKey" in dictionary 
+#[output] TRUE 
+
+#Delete keys and corresponding values from dictionary 
+del(dictionary["VariableKey"]) 
+
+del(dictionary["a"])
+dictionary 
+#[output] {'b':2, 'c':3, 'd':4}
+
+#Lists use ['X', 'Y', 'Z'] or [1, 2, 3] or [1.89, 'y', TRUE]
+#Dictionaries use {"X":1, "Y":2, "Z":3} 
+#Arrays use [1, 2, 3]
+
+#Dicitonary Keys have to be immutable objects 
