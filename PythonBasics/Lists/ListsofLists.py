8d2427d952acef94d713ad5b88b4d795901a1dee
AJ Piter <amanda.piter@gmail.com>
2021-01-25 21:42:36 -0500

    Create ListsofLists.py

---
+++ PythonBasics/Lists/ListsofLists.py
@@ -0,0 +1,7 @@
+#Lists can hold anytype of data including other lists 
+
+ListOfList =  [["ListTitle", variable, TRUE],
+              ["Grade", 100, 75],
+              ["HoursStudied" 8, 4], 
+              ["WatchedVideos", TRUE, TRUE],
+              ["AttendedClass", FALSE, TRUE]]
