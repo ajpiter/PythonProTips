367f81ab6c5ac831d585a9f64f2b8b931b53d0c8
AJ Piter <amanda.piter@gmail.com>
2021-01-26 00:06:29 -0500

    Update BooleanOperators.py

--- PythonBasics/BooleanOperators.py
+++ PythonBasics/BooleanOperators.py
@@ -1,4 +1,7 @@
 #Boolean operators are and, or, not 
+#Numpy also uses logical_and(), logical_or(), and logical_not()
+
+----- #Example: and -----
 
 True and True 
 #output is True 
@@ -13,6 +16,7 @@ x = 12
 x > 5 and x < 15 
 #output is True 
 
+----- #Example: or ----- 
 True or True 
 #output is True 
 
@@ -25,6 +29,8 @@ False or False
 y = 5 
 y < 7 or y > 13 
 #output is True 
+
+----- #Example: not ----- 
 
 not True 
 #output is False 
