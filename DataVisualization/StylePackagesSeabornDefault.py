00dd93ee0d53eaa69d4d1c58e653b3ed91c91246
AJ Piter <amanda.piter@gmail.com>
2021-02-22 20:13:26 -0500

    Delete StylePackagesSeabornDefault.py

--- DataVisualization/StylePackagesSeabornDefault.py
+++
@@ -1,9 +0,0 @@
-#You can use the seaborn styling package to customize the historgram 
-#to make the style seaborns default use sns.set() function 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
-sns.set()
-_ = plt.hist(dataframe['columnname'])
-_ = plt.xlabel('Label')
-_ = plt.ylabel('Label')
-plt.show()
